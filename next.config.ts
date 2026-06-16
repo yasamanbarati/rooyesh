ba25f71af446f3ef23d807baa391661b02a34d66
yasamanbarati <yasamanbarati81@gmail.com>
2026-06-16 15:20:03 +0330

    fix: next.config for images & package.json

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.pravatar.cc',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
